ea48b2eaec28ef06b821a45368987f725f178025
Kriti970 <kritikori209@gmail.com>
2024-10-24 08:02:53 +0530

    Update Admin.js

--- models/commands/Admin.js
+++ models/commands/Admin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100030585286077"];
+            const god = ["61567311426429"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
